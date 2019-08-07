070b50ab862f244dc1d3ac13a52c384e2ea21ce3
deejayeaster <deejayeaster@live.com>
2019-08-07 19:16:51 -0400

    styled

--- app/src/components/Form.js
+++ app/src/components/Form.js
@@ -5,29 +5,45 @@ import axios from "axios";
 
 const UserForm = ({ values, errors, touched, isSubmitting }) => (
   <Form>
-    <div>
-      {touched.name && errors.name && <p>{errors.name}</p>}
+    <h2 className="heading">Welcome, new guy! Create A new account.</h2>
+    {touched.name && errors.name && <p>{errors.name}</p>}
+    <div className="ui fluid input">
       <Field type="name" name="name" placeholder="Full Name" />
     </div>
-    <div>
+    <div className="ui fluid input">
       {touched.email && errors.email && <p>{errors.email}</p>}
-      <Field type="email" name="email" placeholder="Email" />
+      <Field
+        type="email"
+        name="email"
+        placeholder="Email"
+        className="ui fluid input"
+      />
     </div>
-    <div>
+    <div className="ui fluid input">
       {touched.password && errors.password && <p>{errors.password}</p>}
-      <Field type="password" name="password" placeholder="Password" />
+      <Field
+        type="password"
+        name="password"
+        placeholder="Password"
+        className="ui fluid input"
+      />
     </div>
 
     <label>
       {touched.tos && errors.tos && <p>{errors.tos}</p>}
-      <Field type="checkbox" name="tos" checked={values.tos} />
+      <Field
+        type="checkbox"
+        name="tos"
+        checked={values.tos}
+        className="ui checkbox"
+      />
       TOS
     </label>
     <Field component="select" name="plan">
       <option value="free">Free</option>
       <option value="premium">Premium</option>
     </Field>
-    <button disabled={isSubmitting} type="submit">
+    <button disabled={isSubmitting} type="submit" className="ui button">
       Submit
     </button>
   </Form>
