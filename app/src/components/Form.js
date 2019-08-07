1c8ca286b7e200994a9ea2a592b0ae750ee4dd21
deejayeaster <deejayeaster@live.com>
2019-08-07 19:45:49 -0400

    added the post response to state

--- app/src/components/Form.js
+++ app/src/components/Form.js
@@ -1,53 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
-const UserForm = ({ values, errors, touched, isSubmitting }) => (
-  <Form>
-    <h2 className="heading">Welcome, new guy! Create A new account.</h2>
-    {touched.name && errors.name && <p>{errors.name}</p>}
-    <div className="ui fluid input">
-      <Field type="name" name="name" placeholder="Full Name" />
-    </div>
-    <div className="ui fluid input">
-      {touched.email && errors.email && <p>{errors.email}</p>}
-      <Field
-        type="email"
-        name="email"
-        placeholder="Email"
-        className="ui fluid input"
-      />
-    </div>
-    <div className="ui fluid input">
-      {touched.password && errors.password && <p>{errors.password}</p>}
-      <Field
-        type="password"
-        name="password"
-        placeholder="Password"
-        className="ui fluid input"
-      />
-    </div>
+const UserForm = ({ values, errors, touched, isSubmitting, status }) => {
+  const [users, setUsers] = useState([]);
 
-    <label>
-      {touched.tos && errors.tos && <p>{errors.tos}</p>}
-      <Field
-        type="checkbox"
-        name="tos"
-        checked={values.tos}
-        className="ui checkbox"
-      />
-      TOS
-    </label>
-    <Field component="select" name="plan">
-      <option value="free">Free</option>
-      <option value="premium">Premium</option>
-    </Field>
-    <button disabled={isSubmitting} type="submit" className="ui button">
-      Submit
-    </button>
-  </Form>
-);
+  useEffect(() => {
+    if (status) {
+      console.log("post users status: ", status);
+      setUsers([...users, status]);
+    }
+  }, [status]);
+  return (
+    <Form>
+      <h2 className="heading">Welcome, new guy! Create A new account.</h2>
+      {touched.name && errors.name && <p>{errors.name}</p>}
+      <div className="ui fluid input">
+        <Field type="name" name="name" placeholder="Full Name" />
+      </div>
+      <div className="ui fluid input">
+        {touched.email && errors.email && <p>{errors.email}</p>}
+        <Field
+          type="email"
+          name="email"
+          placeholder="Email"
+          className="ui fluid input"
+        />
+      </div>
+      <div className="ui fluid input">
+        {touched.password && errors.password && <p>{errors.password}</p>}
+        <Field
+          type="password"
+          name="password"
+          placeholder="Password"
+          className="ui fluid input"
+        />
+      </div>
+
+      <label>
+        {touched.tos && errors.tos && <p>{errors.tos}</p>}
+        <Field
+          type="checkbox"
+          name="tos"
+          checked={values.tos}
+          className="ui checkbox"
+        />
+        TOS
+      </label>
+      <Field component="select" name="plan">
+        <option value="free">Free</option>
+        <option value="premium">Premium</option>
+      </Field>
+      <button disabled={isSubmitting} type="submit" className="ui button">
+        Submit
+      </button>
+    </Form>
+  );
+};
 
 const FormikApp = withFormik({
   mapPropsToValues({ name, email, password, tos, plan }) {
@@ -71,14 +81,12 @@ const FormikApp = withFormik({
     tos: Yup.boolean().oneOf([true], "Must Accept Terms and Conditions")
   }),
 
-  handleSubmit(values, { setErrors, resetForm, setSubmitting }) {
+  handleSubmit(values, { setErrors, resetForm, setSubmitting, setStatus }) {
     axios
-      .post("https://reqres.in/api/users", {
-        email: values.email,
-        password: values.password
-      })
+      .post("https://reqres.in/api/users", values)
       .then(response => {
         console.log(response);
+        setStatus(response.data);
         window.alert(
           `Created an account for ${response.data.email} with an id number of ${
             response.data.id
